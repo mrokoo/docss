c00e0d0fac9b0bc2392981b9aa2277f7b329dd29
mrokoo <mrokoo@users.noreply.github.com>
2022-10-08 10:52:55 +0000

    deploy: f547f24b7e288bbeb8c3e3c90eb7504350e40a28

--- assets/gettingStart.md.a3ee84e7.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as a,a as p}from"./app.9a1c408f.js";const S=JSON.parse('{"title":"getting Start","description":"","frontmatter":{},"headers":[],"relativePath":"gettingStart.md"}'),r={name:"gettingStart.md"},n=p("",12),_=[n];function s(o,i,c,d,g,f){return a(),e("div",null,_)}const l=t(r,[["render",s]]);export{S as __pageData,l as default};
